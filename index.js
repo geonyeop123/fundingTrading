5caed66892377f8a67c20d929425cc8a38864f29
YEOP <64470512+geonyeop123@users.noreply.github.com>
2021-08-30 23:11:19 +0900

    start
    start

--- index.js
+++ index.js
@@ -1,16 +1,67 @@
 "use strict";
 const upbitPrice = document.querySelector("#upbitPrice");
-console.log(upbitPrice);
-const request = new XMLHttpRequest();
-const url = "https://api.upbit.com/v1/ticker?markets=KRW-XRP";
+const upbitInput = document.querySelector("#upbitInput");
+const upbitValue = document.querySelector("#upbitValue");
+const binancePrice = document.querySelector("#binancePrice");
+const binanceInput = document.querySelector("#binanceInput");
+const binanceValue = document.querySelector("#binanceValue");
+
+const upbitSession = "upbit";
+const binanceSession = "binance";
+let upbitSessionFlag =
+  localStorage.getItem(upbitSession) != null ? true : false;
+let binanceSessionFlag =
+  localStorage.getItem(binanceSession) != null ? true : false;
+let upbit = 0;
+let upbitAmount = 0;
+let binance = 0;
+let binanceAmount = 0;
+
 function getPrice() {
+  const request = new XMLHttpRequest();
+  const url = "https://api.upbit.com/v1/ticker?markets=KRW-XRP";
   request.open("GET", url, false);
   request.send();
   var obj = JSON.parse(request.responseText);
-  const price = obj[0].trade_price;
-  console.log(price);
-  upbitPrice.textContent = price.toLocaleString() + "₩";
+  upbit = obj[0].trade_price;
+  upbitPrice.textContent = upbit.toLocaleString() + "₩";
+  if (upbitSessionFlag) calcuValue(true);
+  if (binanceSessionFlag) calcuValue(true);
 }
 
+function calcuValue(type) {
+  if (type) {
+    upbitAmount = upbitInput.value;
+    upbitValue.textContent = (upbitAmount * upbit).toLocaleString() + "₩";
+  } else {
+    binanceAmount = binanceInput.value;
+    binanceValue.textContent = (binanceAmount * binance).toLocaleString() + "₩";
+  }
+  saveSession(type);
+}
+
+function saveSession(type) {
+  let coin = null;
+  if (type) {
+    localStorage.setItem(upbitSession, upbitAmount);
+    upbitSessionFlag = true;
+    coin = localStorage.getItem(upbitSession);
+  } else {
+    localStorage.setItem(binanceSession, binanceAmount);
+    binanceSessionFlag = true;
+    coin = localStorage.getItem(binanceSession);
+  }
+}
+
+function init() {
+  if (upbitSessionFlag) {
+    upbitInput.value = parseInt(localStorage.getItem(upbitSession));
+    calcuValue(true);
+  }
+  if (binanceSessionFlag) {
+    binanceInput.value = localStorage.getItem(binanceSession);
+    calcuValue(false);
+  }
+}
+init();
 setInterval(getPrice, 1000);
-getPrice();
